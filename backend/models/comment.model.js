83396c64124016adca596abb8a6af8792c19eed0
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-13 12:48:11 +0530

    created comment model

---
+++ backend/models/comment.model.js
@@ -0,0 +1,27 @@
+import mongoose, { now } from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    comment: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    tweetId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tweet",
+    },
+    commentLikes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }
+    ],
+
+}, { timestamps: true });
+
+
+const Comment = mongoose.model("Comment", commentSchema);;
+export default Comment;
