bcf65915a43c24171a10e1201b015afe228a554c
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-14 23:39:52 +0530

    dependencies added

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import { connectDB } from './config/database.js';
+import authRoutes from './routes/auth.routes.js'
 
 
 
 // Configuration
-
+dotenv.config({});
+connectDB();
 
 
 // Initialization
@@ -15,9 +18,23 @@ const port = process.env.PORT || 5000;
 
 
 // Middlewwares
-// app.use('');
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+}));
 
 
+// Route Middlewares
+app.use('/api/v1/auth', authRoutes);
+
+
+
+
+// Home Route
 app.get("/", (req, res) => {
     return res.status(200).json({
         success: true,
