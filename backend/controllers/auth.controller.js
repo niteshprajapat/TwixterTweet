059c92d6aaa3f3c451f30653a8d6c91c3b84dda6
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-15 23:21:28 +0530

    added forgot password and reset password functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { sendLoginEmail } from "../utils/emailHandler.js";
+import crypto from 'crypto';
+import { sendLoginEmail, sendPasswordResetEmail, sendResetSuccessEmail } from "../utils/emailHandler.js";
 
 
 // register
@@ -124,3 +125,117 @@ export const login = async (req, res) => {
         });
     }
 }
+
+// logout
+export const logout = async (req, res) => {
+    try {
+
+        return res.status(200).cookie("twixter", "", {
+            httpOnly: true,
+            maxAge: 0,
+        }).json({
+            success: true,
+            message: "User LoggedOut Successfully!",
+        })
+
+
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error!",
+        });
+    }
+}
+
+
+// forgotPassword
+export const forgotPassword = async (req, res) => {
+    try {
+        const { email } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User Not Found!",
+            });
+        }
+
+        const resetToken = crypto.randomBytes(40).toString("hex");
+        const resetTokenExpiresAt = new Date(Date.now() + 5 * 60 * 1000);
+
+        user.resetPasswordToken = resetToken;
+        user.resetPasswordTokenExpiresAt = resetTokenExpiresAt;
+
+        await user.save();
+
+        // send email
+        await sendPasswordResetEmail(email, `${process.env.FRONTEND_URL}/resetPassword/${resetToken}`);
+
+        return res.status(200).json({
+            success: true,
+            message: "Password reset link send to your email address!",
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error!",
+        });
+    }
+}
+
+
+
+
+// resetPassword
+export const resetPassword = async (req, res) => {
+    try {
+        const resetToken = req.params.token;
+        const { newPassword } = req.body;
+
+        const user = await User.findOne({ resetPasswordToken: resetToken, resetPasswordTokenExpiresAt: { $gt: Date.now() } });
+
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid or expired reset token!",
+            });
+        }
+
+        // update Password
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+        user.password = hashedPassword;
+        user.resetPasswordToken = undefined;
+        user.resetTokenExpiresAt = undefined;
+
+        await user.save();
+
+        // send success reset email
+        await sendResetSuccessEmail(user.email);
+
+
+        return res.status(200).json({
+            success: true,
+            message: "Password reset successfully!",
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error!",
+        });
+    }
+}
+
+
+
