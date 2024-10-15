c3054146cfe925b260c4c86158b11538abde00fb
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-15 23:22:37 +0530

    wrote email handlers for  forgot and reset password

--- backend/utils/emailHandler.js
+++ backend/utils/emailHandler.js
@@ -20,7 +20,7 @@ const transporter = nodemailer.createTransport({
 
 
 
-
+// sendLoginEmail
 export const sendLoginEmail = async (userEmail) => {
     try {
         const info = await transporter.sendMail({
@@ -39,6 +39,52 @@ export const sendLoginEmail = async (userEmail) => {
         console.log("Error sending login email", error);
     }
 }
+
+
+// sendPasswordResetEmail
+export const sendPasswordResetEmail = async (userEmail, resetURL) => {
+    try {
+        const info = await transporter.sendMail({
+            from: `TwisterTweet <${process.env.EMAIL_USER}> `,
+            to: userEmail,
+            subject: "Login Notification",
+            text: "Reset Password Link.",
+            html: `<p>   
+            <a href=${resetURL}>Reset Link</a>
+            </p>`,
+        });
+
+        console.log("Login mail Sent");
+        console.log(`Login email send: ${info?.messageId}`);
+
+
+    } catch (error) {
+        console.log("Error sending login email", error);
+    }
+}
+
+// sendResetSuccessEmail
+export const sendResetSuccessEmail = async (userEmail) => {
+    try {
+        const info = await transporter.sendMail({
+            from: `TwisterTweet <${process.env.EMAIL_USER}> `,
+            to: userEmail,
+            subject: "Login Notification",
+            text: "Reset Password Link.",
+            html: `<p>   
+            Password Reset Successfully!
+            </p>`,
+        });
+
+        console.log("Login mail Sent");
+        console.log(`Login email send: ${info?.messageId}`);
+
+
+    } catch (error) {
+        console.log("Error sending login email", error);
+    }
+}
+
 
 
 
