352a4f487dce96b4d2471ee93ae584f80e3631d7
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-18 22:59:18 +0530

    suggested user controller added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Notification from "../models/notification.model.js";
 
 
 
@@ -50,6 +51,117 @@ export const getProfileById = async (req, res) => {
             message: "Fteched User by Id",
             user,
         });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error!",
+        });
+    }
+}
+
+// followUnnfollowByID
+export const followUnfollowByID = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const loggedInUserId = req.user._id;
+
+
+        if (userId?.toString() === loggedInUserId?.toString()) {
+            return res.status(400).json({
+                success: false,
+                message: "You cannot follow/unfollow yourself!",
+            });
+        }
+
+        const user = await User.findById(userId).select("-password");
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "User Not Found!"
+            });
+        }
+
+
+        const loggedInUser = await User.findById(loggedInUserId).select("-password");
+
+
+        if (loggedInUser?.following?.includes(userId)) {
+            // unfollow
+
+            await User.findByIdAndUpdate(loggedInUserId, { $pull: { following: userId } }, { new: true });
+            await User.findByIdAndUpdate(userId, { $pull: { followers: loggedInUserId } }, { new: true });
+
+            return res.status(200).json({
+                success: true,
+                message: `${loggedInUser?.userName} unfollowed ${user?.userName}`,
+            });
+
+        } else {
+            // follow
+
+            await User.findByIdAndUpdate(loggedInUserId, { $push: { following: userId } }, { new: true });
+            await User.findByIdAndUpdate(userId, { $push: { followers: loggedInUserId } }, { new: true });
+
+
+
+            // send notifications and email
+            const notification = new Notification({
+                from: loggedInUserId,
+                to: userId,
+                type: "follow",
+            });
+
+            await notification.save();
+
+            return res.status(200).json({
+                success: true,
+                message: `${loggedInUser?.userName} followed ${user?.userName}`,
+            });
+
+        }
+
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error!",
+        });
+    }
+}
+
+
+// getSuggestedUsers
+export const getSuggestedUsers = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const userFollowedByMe = await User.findById(userId).select("following");
+
+
+        const users = await User.aggregate([
+            {
+                $match: {
+                    _id: { $ne: userId },
+                }
+            },
+            { $sample: { size: 10 } },
+        ]);
+
+
+        const filteredUser = users.filter((user) => !userFollowedByMe.following.includes(user?._id));
+        const suggestedUsers = filteredUser?.slice(5);
+
+        suggestedUsers?.forEach((user) => (user.password = null));
+
+        return res.status(200).json({
+            success: true,
+            message: "All Suggested Users Fetched Successfully.",
+            suggestedUsers,
+        })
+
 
     } catch (error) {
         console.log(error);
