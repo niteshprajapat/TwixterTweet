809253f198a9b757b52dd65e16c43d79ad0e411f
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-18 23:00:07 +0530

    suggested user API created

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
-import { getProfileById, meProfile } from '../controllers/user.controller.js';
+import { followUnfollowByID, getProfileById, meProfile, getSuggestedUsers } from '../controllers/user.controller.js';
 
 
 router.get("/me", isAuthenticated, meProfile);
 router.get("/profile/:userId", isAuthenticated, getProfileById);
+
+router.get("/followUnfollow/:id", isAuthenticated, followUnfollowByID);
+router.get("/suggestedUsers", isAuthenticated, getSuggestedUsers);
 
 
 export default router;
