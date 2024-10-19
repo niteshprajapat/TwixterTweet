98ecc291ec2efeb9165247ee4debe616bbbf17e0
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-19 22:15:18 +0530

    apis created for suggestedUsers, followers and following lists

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
-import { followUnfollowByID, getProfileById, meProfile, getSuggestedUsers } from '../controllers/user.controller.js';
+import { followUnfollowByID, getProfileById, meProfile, getSuggestedUsers, searchAccount, deleteAccount, followingList, followersList } from '../controllers/user.controller.js';
 
 
 router.get("/me", isAuthenticated, meProfile);
@@ -11,6 +11,17 @@ router.get("/profile/:userId", isAuthenticated, getProfileById);
 
 router.get("/followUnfollow/:id", isAuthenticated, followUnfollowByID);
 router.get("/suggestedUsers", isAuthenticated, getSuggestedUsers);
+
+router.get('/search', isAuthenticated, searchAccount);
+
+
+router.get("followingList/:id", isAuthenticated, followingList)
+router.get("followersList/:id", isAuthenticated, followersList)
+
+
+router.delete('/deleteAccount', isAuthenticated, deleteAccount);
+
+// Update User - Pending
 
 
 export default router;
