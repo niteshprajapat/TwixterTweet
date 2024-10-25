76b7dad1c9b5136e47fae17da781dae95b205c01
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-25 16:41:06 +0530

    Google Auth routes added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 import { register, login, logout, forgotPassword, resetPassword } from '../controllers/auth.controller.js';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
+import passport from 'passport';
 
 
 router.post("/register", register);
@@ -12,6 +13,15 @@ router.get("/logout", isAuthenticated, logout);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password/:token', resetPassword);
 
-// Google OAuth
+
+// Google OAuth routes
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+router.get('/auth/google/callback', passport.authenticate('google', {
+    failureRedirect: '/login',
+    successRedirect: '/', // Adjust this redirect URL as needed
+})
+);
 
 export default router;
