1c291ceb329bc4a0aca72f1a2fa9898d4dbeece0
Nitesh Prajapat <niteshp282000@gmail.com>
2024-11-20 20:47:26 +0530

    Bookmark tweet api created

--- backend/routes/tweet.routes.js
+++ backend/routes/tweet.routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
-import { createTweet, deleteTweet, likeUnlikeTweet, updateTweet } from '../controllers/tweet.controller.js';
+import { bookmarkTweet, createTweet, deleteTweet, likeUnlikeTweet, updateTweet } from '../controllers/tweet.controller.js';
 
 
 router.post("/createTweet", isAuthenticated, createTweet);
@@ -11,6 +11,7 @@ router.put("/updateTweet/:id", isAuthenticated, updateTweet);
 
 router.get("/likeUnlikeTweet/:id", isAuthenticated, likeUnlikeTweet);
 
+router.get("/bookmark-tweet/:tweetId", isAuthenticated, bookmarkTweet);
 export default router;
 
 
