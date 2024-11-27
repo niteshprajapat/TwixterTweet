70ee6ac1dbf3c090d1e801bd67b30081f2de7d0c
Nitesh Prajapat <niteshp282000@gmail.com>
2024-11-27 18:53:34 +0530

    API Routes created for fetcAllTweets, followingTweets, likedTweetByUserId, and retweet

--- backend/routes/tweet.routes.js
+++ backend/routes/tweet.routes.js
@@ -2,16 +2,27 @@ import express from 'express';
 const router = express.Router();
 
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
-import { bookmarkTweet, createTweet, deleteTweet, likeUnlikeTweet, updateTweet } from '../controllers/tweet.controller.js';
+import { bookmarkTweet, createTweet, deleteTweet, fetchAllTweets, followingTweetsOnly, likedTweetByUserId, likeUnlikeTweet, retweetTweet, updateTweet } from '../controllers/tweet.controller.js';
 
 
+
+router.get("/fetchAllTweets", isAuthenticated, fetchAllTweets);
 router.post("/createTweet", isAuthenticated, createTweet);
 router.delete("/deleteTweet/:id", isAuthenticated, deleteTweet);
 router.put("/updateTweet/:id", isAuthenticated, updateTweet);
-
 router.get("/likeUnlikeTweet/:id", isAuthenticated, likeUnlikeTweet);
-
 router.get("/bookmark-tweet/:tweetId", isAuthenticated, bookmarkTweet);
+router.get("/followingTweets", isAuthenticated, followingTweetsOnly);
+
+router.get("/likedTweetByUserId/:userId", isAuthenticated, likedTweetByUserId)
+
+
+
+// RE-Tweet Functionality
+
+// retweet a tweet
+router.post("/retweet/:tweetId", isAuthenticated, retweetTweet);
+
 export default router;
 
 
