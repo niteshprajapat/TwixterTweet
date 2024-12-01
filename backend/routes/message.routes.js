7484835246978bb62c3f9cf981fc9ce84f83fcdb
Nitesh Prajapat <niteshp282000@gmail.com>
2024-12-01 18:53:21 +0530

    delete message api route created

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -2,12 +2,13 @@ import express from 'express';
 const router = express.Router();
 
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
-import { getMessages, sendMessage, updateMessage } from '../controllers/message.controller.js';
+import { deleteMessage, getMessages, sendMessage, updateMessage } from '../controllers/message.controller.js';
 
 
 router.post("/sendMessage/:id", isAuthenticated, sendMessage);
 router.get("/getMessages/:id", isAuthenticated, getMessages);
 router.put("/updateMessage/:id", isAuthenticated, updateMessage);
+router.delete("/deleteMessage/:id", isAuthenticated, deleteMessage);
 
 
 
