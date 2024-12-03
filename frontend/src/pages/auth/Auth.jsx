311a0f0bbe6563a79e713de73f8c3d485519095d
Nitesh Prajapat <niteshp282000@gmail.com>
2024-12-03 23:40:39 +0530

    auth page and register functionality

---
+++ frontend/src/pages/auth/Auth.jsx
@@ -0,0 +1,142 @@
+import React, { useState } from 'react';
+
+
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/components/ui/tabs"
+import { useMutation } from '@tanstack/react-query';
+import { routes } from '@/routes/route';
+import axios from 'axios';
+
+
+const Auth = () => {
+    const [fullname, setFullname] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const { mutate: registerMutate, isError, error, isPending } = useMutation({
+        mutationFn: async ({ }) => {
+            const response = await axios.post(routes.REGISTER, { fullname, username, email, password }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                withCredentials: true,
+            });
+
+            const data = await response.data;
+            return data;
+        },
+        onSuccess: (data) => {
+            console.log("DATA", data);
+        },
+        onError: (error) => {
+            console.log("error", error);
+        }
+    });
+
+
+
+
+    return (
+        <div className='w-full h-screen bg-[#09090B] flex justify-center items-center'>
+
+            <Tabs defaultValue="register" className="w-[400px]  ">
+                <TabsList className="grid w-full grid-cols-2 bg-[#09090B]">
+                    <TabsTrigger value="register">Register</TabsTrigger>
+                    <TabsTrigger value="login">Login</TabsTrigger>
+                </TabsList>
+                <TabsContent value="register">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Register</CardTitle>
+                            <CardDescription>
+                                Make changes to your account here. Click save when you're done.
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="space-y-2">
+                            <div className="space-y-1">
+                                <Label htmlFor="name">Fullname</Label>
+                                <Input
+                                    value={fullname}
+                                    onChange={(e) => setFullname(e.target.value)}
+                                    type="text"
+                                />
+                            </div>
+                            <div className="space-y-1">
+                                <Label htmlFor="username">Username</Label>
+                                <Input
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    type="text"
+                                />
+                            </div>
+                            <div className="space-y-1">
+                                <Label htmlFor="username">Email</Label>
+                                <Input
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    type="email"
+                                />
+                            </div>
+                            <div className="space-y-1">
+                                <Label htmlFor="username">Password</Label>
+                                <Input
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    type="password"
+                                />
+                            </div>
+                        </CardContent>
+                        <CardFooter>
+                            <Button onClick={() => registerMutate(fullname, username, email, password)}>Save changes</Button>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+                <TabsContent value="login">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Login</CardTitle>
+                            <CardDescription>
+                                Change your password here. After saving, you'll be logged out.
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="space-y-2">
+                            <div className="space-y-1">
+                                <Label htmlFor="current">Username or Email</Label>
+                                <Input id="current" type="text" />
+                            </div>
+                            <div className="space-y-1">
+                                <Label htmlFor="new">Password</Label>
+                                <Input id="new" type="password" />
+                            </div>
+                        </CardContent>
+                        <CardFooter>
+                            <Button>Save password</Button>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+            </Tabs>
+        </div>
+
+    )
+}
+
+export default Auth;
+
+
