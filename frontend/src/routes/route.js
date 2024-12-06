5f82d6f98d9a9437505f10dedd7b833d0b710ae8
Nitesh Prajapat <niteshp282000@gmail.com>
2024-12-06 21:26:53 +0530

    create js file or all routes

--- frontend/src/routes/route.js
+++ frontend/src/routes/route.js
@@ -2,4 +2,8 @@ const BASE_CASE = "http://127.0.0.1:5000/api/v1"
 
 export const routes = {
     REGISTER: `${BASE_CASE}/auth/register`,
+    LOGIN: `${BASE_CASE}/auth/login`,
+    LOGOUT: `${BASE_CASE}/auth/logout`,
+    MEPROFILE: `${BASE_CASE}/user/me`,
 }
+
