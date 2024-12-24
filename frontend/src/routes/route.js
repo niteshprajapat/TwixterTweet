afab4314440d3f24b89c8ef2580c4d9b7c7be28b
Nitesh Prajapat <niteshp282000@gmail.com>
2024-12-24 11:19:25 +0530

    new routes added

--- frontend/src/routes/route.js
+++ frontend/src/routes/route.js
@@ -8,17 +8,24 @@ export const routes = {
 
     profileByUserId: `${BASE_URL}/user/profile`, // /:userId
 
-    uploadFile: `${BASE_URL}/user/uploadFile`,   //http://127.0.0.1:5000/api/v1/user/uploadFile
+    uploadFile: `${BASE_URL}/user/uploadFile`,
+
+    searchAccount: `${BASE_URL}/user/search`,  // ?query=    
 
 
     // tweets routes
 
     createTweet: `${BASE_URL}/tweet/createTweet`,
     fetchAllTweets: `${BASE_URL}/tweet/fetchAllTweets`,
+    fetchTweetByTweetId: `${BASE_URL}/tweet/tweetsById`, // /:tweetId
 
     tweetByUserID: `${BASE_URL}/tweet/tweetsByUserId`, // /:userId
     tweetLikedByUserID: `${BASE_URL}/tweet/likedTweetByUserId`, // /:userId
     likeUnlikeTweetByTweetID: `${BASE_URL}/tweet/likeUnlikeTweet`, // /:tweetId 
     deleteTweetByTweetID: `${BASE_URL}/tweet/deleteTweet`, // /:tweetId
+
+    bookMarkTweet: `${BASE_URL}/tweet/bookmark-tweet`, // /:tweetId
+    fetchAllBookMarkTweets: `${BASE_URL}/tweet/get-bookmark-tweets`,
+
 }
 
