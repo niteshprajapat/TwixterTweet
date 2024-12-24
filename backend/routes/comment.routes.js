ba57bbeb2b9d1f5de14caa95dd0db4f84f91b796
Nitesh Prajapat <niteshp282000@gmail.com>
2024-12-24 17:05:25 +0530

    api created for comment by tweetId

--- backend/routes/comment.routes.js
+++ backend/routes/comment.routes.js
@@ -3,13 +3,16 @@ const router = express.Router();
 
 
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
-import { commentLikeUnlike, createComment, deleteComment, updateComment } from '../controllers/comment.controller.js';
+import { commentLikeUnlike, createComment, deleteComment, fetchCommentsByTweetId, updateComment } from '../controllers/comment.controller.js';
 
 
 router.post('/createComment/:tweetId', isAuthenticated, createComment);
 router.put('/updateComment/:commentId', isAuthenticated, updateComment);
 router.delete('/deleteComment/:commentId', isAuthenticated, deleteComment);
 router.get('/commentLikeUnlike/:commentId', isAuthenticated, commentLikeUnlike);
+router.get('/fetchCommentsByTweetId/:tweetId', isAuthenticated, fetchCommentsByTweetId);
+
+
 
 
 
