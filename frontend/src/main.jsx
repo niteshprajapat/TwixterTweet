e0f608e04446239041054cecd79482311c16db64
Nitesh Prajapat <niteshp282000@gmail.com>
2024-12-31 23:47:58 +0530

    working on frontend side

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,22 @@ import './index.css'
 import App from './App.jsx';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from './components/ui/sonner';
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+        }
+    }
+});
 
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
             <App />
+            <Toaster />
         </QueryClientProvider>
     </StrictMode>,
 )
